8a17df0752bc854ca7a8f1fec7e58a1309f5bd18
amaargiru <war4one@gmail.com>
2022-04-24 21:19:08 +0500

    Fix error + update frontend

--- Code/Back/Api/api.py
+++ Code/Back/Api/api.py
@@ -1,10 +1,9 @@
+import json
 import sys
 
 from fastapi import FastAPI
 
 sys.path.append('../Core')
-
-import json
 
 from printer import Printer
 from comparator import Comparator
@@ -16,6 +15,7 @@ from refiner import Refiner
 app = FastAPI()
 
 
+# http://127.0.0.1:8000/docs
 @app.get("/")
 def read_root():
     return {"Hello!": "Please read the docs: http://127.0.0.1:8000/docs"}
@@ -25,7 +25,7 @@ def read_root():
 @app.get("/get_next_question")
 def get_next_question(user_id: int):
     question_id = Examiner.define_next_question_num(user_id)
-    question = Examiner.next_question(question_id)
+    question = Examiner.get_question(question_id)
 
     return {"question_id": question_id,
             "native_phrase": f"{question.native_phrase}"}
@@ -39,10 +39,8 @@ def get_answer_check(user_id: int, question_id: int, user_input: str):
     user_input_complex = Complicator.complicate_user_input(user_input_cleaned)
     user_input_without_punctuation_lower = Lower.list_lower(user_input_complex.user_input_without_punctuation)
     references_lower = Lower.references_lower(question.references)
-
     index, ratio = Comparator.find_nearest_reference_index(user_input_without_punctuation_lower, references_lower)
     correction = Comparator.find_matching_blocks(user_input_without_punctuation_lower, references_lower, index)
     a = Printer.format_message_to_api(question.references, index, correction, ratio)
 
-    return {"question_id": 1,
-            "answer": json.dumps(a)}
+    return {"question_id": 1,            "answer": json.dumps(a)}

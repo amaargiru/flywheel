e3320cbcf372505749e8e8878d7b6b01706c062c
amaargiru <war4one@gmail.com>
2022-05-03 20:09:14 +0500

    Update front

--- Code/Back/api.py
+++ Code/Back/api.py
@@ -30,16 +30,6 @@ SECRET_KEY = "f6e936d318b4103412c142893d7331fb83c2228fc31155a8f96de27c3afdeea7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 10 * 365 * 24 * 60
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
 
 @dataclass
 class Token:
@@ -116,21 +106,6 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return LocalUser(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -232,6 +207,7 @@ async def get_answer_check(user_id: int, question_id: int, user_input: str, curr
     a = Printer.format_message_to_api(question.references, index, correction, ratio)
 
     return {"question_id": 1, "answer": json.dumps(a)}
+
 
 if __name__ == "__main__":
     try:

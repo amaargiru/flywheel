7e8223c1d5244334abf75039ab8d2258baa4fa26
amaargiru <war4one@gmail.com>
2023-01-09 14:28:18 +0500

    Add automatic duplicator remover

--- Phrases/phr2json.py
+++ Phrases/phr2json.py
@@ -8,12 +8,16 @@ duplicates_file: str = "duplicates.json"
 phrase_mapping: dict = defaultdict(str)
 duplicates: dict = {}
 
-# Transition from delimited text to JSON
-with (open(phrases_file, "r", encoding="utf-8") as pf,
+with (open(phrases_file, "r+", encoding="utf-8") as pf,
       open(json_file, "w", encoding="utf-8") as jf,
       open(duplicates_file, "w", encoding="utf-8") as df):
     phrases = pf.readlines()
 
+    # Erase the file content before saving w/o duplicates
+    pf.truncate(0)
+    pf.seek(0)
+
+    # Transition from delimited text to JSON
     for phrase in phrases:
         if "|" in phrase:
             phrases_list = list(map(str.strip, phrase.split("|")))
@@ -22,10 +26,16 @@ with (open(phrases_file, "r", encoding="utf-8") as pf,
 
             if phrase_mapping[phrase_rus] != "":  # Search for duplicates
                 duplicates[phrase_rus] = phrases_eng
+            else:
+                phrase_mapping[phrase_rus] = phrases_eng
+                # Save delimited text w/o duplicates (text cleanup)
+                # Text is compiled from different sources and definitely contains duplicates
+                pf.write(phrase)
 
-            phrase_mapping[phrase_rus] = phrases_eng
-
+    # Save JSON
     if len(phrase_mapping) > 0:
         json.dump(phrase_mapping, jf, ensure_ascii=False, indent=2)
+
+    # Save list of duplicates (for info purposes only)
     if len(duplicates) > 0:
         json.dump(duplicates, df, ensure_ascii=False, indent=2)

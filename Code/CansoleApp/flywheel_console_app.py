60bb03671b4f437b630789997300d6516d0f3838
amaargiru <war4one@gmail.com>
2023-01-12 11:37:40 +0500

    Update the ability to work with multiple phrases

--- Code/CansoleApp/flywheel_console_app.py
+++ Code/CansoleApp/flywheel_console_app.py
@@ -9,9 +9,10 @@ duplicates_file: str = "duplicates.json"
 
 phrase_mapping: dict = defaultdict(str)
 duplicates_and_errors: list = []
+phrases_without_diplicates: list = []
 
 
-def find_file_in_project_directories(file_name):
+def find_file_in_project_directory(file_name):
     if os.path.exists(file_name):
         return file_name
     # If files doesn't exist in app directory, start search in all project directories
@@ -21,19 +22,15 @@ def find_file_in_project_directories(file_name):
                 return os.path.join(root, file_name)
 
 
-with (open(find_file_in_project_directories(phrases_file), "r+", encoding="utf-8") as phrf,
-      open(find_file_in_project_directories(json_file), "w", encoding="utf-8") as jsonf,
-      open(find_file_in_project_directories(duplicates_file), "w", encoding="utf-8") as dupf):
+with (open(find_file_in_project_directory(phrases_file), "r+", encoding="utf-8") as phrf,
+      open(find_file_in_project_directory(json_file), "w", encoding="utf-8") as jsonf,
+      open(find_file_in_project_directory(duplicates_file), "w", encoding="utf-8") as dupf):
     phrases_file_content = phrf.readlines()  # Phrases + comments
-
-    # Erase the file content before saving without duplicates
-    phrf.truncate(0)
-    phrf.seek(0)
 
     # Transition from delimited text to JSON
     for string in phrases_file_content:
         if string[0] == "#" or string == "\n":
-            phrf.write(string)  # Just save comment string or empty string
+            phrases_without_diplicates.append(string)  # Just save comment string or empty string
         elif "||" in string:
             phrases_list = list(map(str.strip, string.split("||")))
             if len(phrases_list) > 2:
@@ -47,26 +44,35 @@ with (open(find_file_in_project_directories(phrases_file), "r+", encoding="utf-8
 
                 if "|" not in rus_part:  # Single russian phrase
                     if phrase_mapping[rus_part] != "":  # Search for duplicates
-                        duplicates_and_errors.append(f"Duplicate: {string}")
+                        duplicates_and_errors.append(f"Duplicate: '{string}'")
                     else:
                         phrase_mapping[rus_part] = eng_part
                         # Save delimited text without duplicates (text cleanup)
                         # Text is compiled from different sources and definitely contains duplicates
-                        phrf.write(string)
+                        phrases_without_diplicates.append(string)
 
                 else:  # Multiple russian phrases
                     rus_part = list(map(str.strip, rus_part.split("|")))  # Split into separate russian phrases...
                     for rus_phrase in rus_part:
                         if phrase_mapping[rus_phrase] == eng_part:  # Search for duplicates
-                            duplicates_and_errors.append(f"Duplicate: {string}")
+                            duplicates_and_errors.append(f"Duplicate: '{rus_phrase}' in '{string}'")
                         else:
                             phrase_mapping[rus_phrase] = eng_part  # ... and save separate items
                             # Save delimited text without duplicates (text cleanup)
                             # Text is compiled from different sources and definitely contains duplicates
-                            phrf.write(string)
+                            if string not in phrases_without_diplicates:
+                                phrases_without_diplicates.append(string)
 
         else:
-            duplicates_and_errors.append("Error. String doesn't contains '||' delimeter")
+            duplicates_and_errors.append(f"Error. '{string}' doesn't contains '||' delimeter")
+
+    # Erase the file content before saving
+    phrf.truncate(0)
+    phrf.seek(0)
+
+    # Save phrases without duplicates
+    for string in phrases_without_diplicates:
+        phrf.write(string)
 
     # Save JSON
     if len(phrase_mapping) > 0:

63aa1ed21d06d13255098c01e7bec9508212439a
amaargiru <war4one@gmail.com>
2023-01-16 18:53:03 +0500

    Add read_phrases()

--- Code/system_level.py
+++ Code/system_level.py
@@ -19,7 +19,31 @@ class FileOperations:
 
     @staticmethod
     def read_phrases(file_path: str) -> dict:
-        ...
+        phrase_mapping: dict = {}
+
+        with open(file_path, 'r', encoding='utf-8') as phrf:
+            for string in phrf.readlines():
+                if string[0] != '#' and '||' in string:  # No comment line and contains rus-eng separator
+                    phrases_pair = list(map(str.strip, string.split("||")))
+
+                    if len(phrases_pair) > 2:
+                        print(f'Error. String contains {len(phrases_pair)} "||" separators: ' + string +
+                              '. String must contain only one "||" separator between phrases in different languages')
+                    else:
+                        rus_part, eng_part = phrases_pair[0], phrases_pair[1]
+
+                        if "|" in eng_part:  # Multiple english phrases
+                            eng_part = list(map(str.strip, eng_part.split('|')))  # Just split into separate english phrases
+
+                        if "|" in rus_part:  # Multiple russian phrases
+                            rus_part = list(map(str.strip, rus_part.split('|')))  # Split into separate russian phrases...
+                            for rus_phrase in rus_part:
+                                phrase_mapping[rus_phrase] = eng_part  # ... and save separate items
+
+                        else:  # Single russian phrase
+                            phrase_mapping[rus_part] = eng_part
+
+        return phrase_mapping
 
     @staticmethod
     def read_repetitions(file_path: str) -> dict:
